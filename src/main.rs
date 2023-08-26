84364f9d8bf2820b6e86b3bba550aa701a4b89a7
Amadeus8182 <amadeus8182@gmail.com>
2023-08-26 13:16:19 -0500

    allow command-line arguments for paths to files
    If the user does not provide any file paths, it will tell the user to
    provide a file path. Error handling is included for potentially invalid
    or non-existing files.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 mod lexer;
 use lexer::{tokens::Token, lex::Lexer};
+use std::{fs, env};
 fn main() {
-    let mut lex = Lexer::new(String::from("for;int;this_is_an_identifier;....;1234"));
-    loop {
-        let tok = lex.next_token();
-        if let Token::EOF = tok {
-            println!("{:?}", tok);
-            break;
-        } else {
-            println!("{:?}", tok);
-        }
+    let paths: Vec<String> = env::args().skip(1).collect();
+
+    if paths.is_empty() {
+        println!("Please provide a file path!");
     }
 
+    for path in paths {
+        let input = match fs::read_to_string(&path) {
+            Ok(inp) => inp,
+            Err(err) => { println!("{}: {}", &path, err); continue; }
+        };
+
+        let mut lex = Lexer::new(input);
+        loop {
+            let tok = lex.next_token();
+            if let Token::EOF = tok {
+                println!("{:?}", tok);
+                break;
+            } else {
+                println!("{:?}", tok);
+            }
+        }
+    }
 }
