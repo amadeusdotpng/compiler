64aad3edeb4c49b1c402c20077518a2d6a67350f
Amadeus8182 <amadeus8182@gmail.com>
2023-09-05 12:14:07 -0500

    add third python prototype of lexer
    The lexer is now in a class and uses a method to call the next token
    instead of reading the entire input string at once. The code also looks
    much cleaner than the last.

--- src/protopy/lexer.py
+++ src/protopy/lexer.py
@@ -1,45 +1,53 @@
 import re
-s = "while;identifier;1234;"
-# print(f"str length: {len(s)}")
 
-curr_pos = 0
-read_pos = 1
+class Lexer:
+    def __init__(self, input: str):
+        self.input = input
+        self.curr_pos = 0
+        self.read_pos = 1
 
-tokens = []
-tok = "TOKEN::ERROR"
+    def next_token(self) -> (str, str):
+        tok = "TOKEN::ERROR"
 
-def match_string(substring):
-    if re.match(r"^while$", substring):
-        return "TOKEN::WHILE"
-    elif re.match(r"^;$", substring):
-        return "TOKEN::SEMICOLON"
-    elif re.match(r"^[a-zA-Z_]+$", substring):
-        return "TOKEN::IDENTIFIER"
-    else:
-        return "TOKEN::ERROR"
+        if self.read_pos > len(self.input):
+            return ("", "TOKEN::EOF")
+
+        while True:
+            sub = self.input[self.curr_pos:self.read_pos]
+            t_tok = self.match_string(sub)
+
+            if self.read_pos > len(self.input):
+                return (sub, tok) 
+            
+            elif t_tok == "TOKEN::ERROR":
+                if tok == "TOKEN::ERROR":
+                    self.read_pos += 1
+                    self.curr_pos = self.read_pos-1
+                    return (sub, tok)
+                else:
+                    self.curr_pos = self.read_pos-1
+                    return (sub[:-1], tok)
+            else:
+                tok = t_tok
+                self.read_pos += 1
+                
+    
+    def match_string(self, sub: str) -> str:
+        if re.match(r"^while$", sub):
+            return "TOKEN::WHILE"
+        elif re.match(r"^;$", sub):
+            return "TOKEN::SEMICOLON"
+        elif re.match(r"^[a-zA-Z_]+$", sub):
+            return "TOKEN::IDENTIFIER"
+        else:
+            return "TOKEN::ERROR"
+
+
+inp = "while;identifer;32202;"
+lex = Lexer(inp)
 
 while True:
-    t_tok = match_string(s[curr_pos:read_pos])
-#    print((t_tok, s[curr_pos:read_pos], curr_pos, read_pos))
-    if read_pos > len(s):
-        if s[curr_pos:read_pos-1]: 
-            tokens.append((s[curr_pos:read_pos-1], tok))
+    lexeme, token = lex.next_token()
+    print((lexeme, token))
+    if token == "TOKEN::EOF":
         break
-
-    elif t_tok == "TOKEN::ERROR":
-        if tok == "TOKEN::ERROR":
-            tokens.append((s[curr_pos:read_pos], tok))
-            read_pos += 1
-        else:
-            tokens.append((s[curr_pos:read_pos-1], tok))
-
-        curr_pos = read_pos-1;
-        tok = "TOKEN::ERROR"
-    else:
-        tok = t_tok
-        read_pos += 1
-
-
-print()
-for pair in tokens:
-    print(pair)
