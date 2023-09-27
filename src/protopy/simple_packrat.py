af9ebcbe6ef49c2729f9f798aba3c1f0ea8b6dbe
Amadeus8182 <amadeus8182@gmail.com>
2023-09-27 14:44:35 -0500

    added productions for non-terminal mul (WIP)
    debug print statements have been changed to try to be more helpful.
    still buggy and cannot parse expressions such as `1*2+3` for some
    reason.

--- src/protopy/simple_packrat.py
+++ src/protopy/simple_packrat.py
@@ -9,18 +9,15 @@ class Parser:
         self.memo_table = {}
 
     def mark(self):
-        print(f"marking:{self.pointer}")
+        print(f"mark: {self.pointer}")
         return self.pointer
 
     def reset(self, pos):
-        print(f"resetting:{pos}")
+        print(f"reset: {pos}")
         self.pointer = pos
 
-#    def get_memo(self, func):
-#        pass
-
     # use this function in replacement of left-recursive non-terminals in productions
-    def memoize(self, func):
+    def left_rec_memoize(self, func):
         print("enter memoize")
         save = self.mark()
         key = (save, func)
@@ -37,6 +34,7 @@ class Parser:
                 print("enter loop")
                 self.reset(save)
                 res = func()
+                print("in loop")
 
                 # This is where the pointer will be after going through func()
                 endpos = self.mark() 
@@ -45,20 +43,16 @@ class Parser:
                 # don't store the result in the memo_table
                 print(f"loop:\nres:\n{res}\nendpos:{endpos}\nlastres:{lastres}\nlastpos:{lastpos}")
                 if endpos <= lastpos or endpos >= len(self.input):
+                    print("exiting loop")
                     break
 
-                print("caching res\n")
+                print("caching")
                 self.memo_table[key] = lastres, lastpos = res, endpos 
 
             # Make sure to return the proper result
-
-            print("exit loop")
             res = lastres
             self.reset(lastpos)
-
-        print(f"res:\n{res}")
-        print(f"pointer: {self.pointer}")
-        print("returning memoize\n")
+            print(f"returning:\nres:\n{res}\npointer:{self.pointer}")
         return res
 
     def parse(self) -> (bool, PTNode):
@@ -69,12 +63,10 @@ class Parser:
     def term(self, tok):
         print("enter term")
         if self.pointer > len(self.input)-1:
-            print("exiting term")
             return None
         res = self.input[self.pointer][0] == tok
-        print(f"res:{res}, pointer:{self.pointer}, tok:{tok}, inptok:{self.input[self.pointer][0]}")
+        print(f"{self.input[self.pointer][0]}=={tok}:{res}")
         self.pointer += 1
-
         print("exiting term")
         if res:
             return tok
@@ -86,45 +78,90 @@ class Parser:
 
         self.reset(save)
         if (prod:=self.add_0()):
+            print("in add: returning add_0")
             return PTNode("add_0", prod)
         self.reset(save)
         if (prod:=self.add_1()):
+            print("in add: returning add_1")
             return PTNode("add_1", prod)
         self.reset(save)
         if (prod:=self.add_2()):
+            print("in add: returning add_2")
             return PTNode("add_2", prod)
+        self.reset(save)
+        print("in add: returning None")
         return None
 
     def add_0(self):
         print("enter add_0")
-        res = bool((add0 := self.memoize(self.add))
-            and(tok0 := self.term(Token.ADD))
-            and(atom0:= self.atom()))
-        print("in add_0")
-        print(f"res:{res}")
-        print("exiting add_0\n")
+        res = bool(   (add0 := self.left_rec_memoize(self.add))
+                   and(tok0 := self.term(Token.ADD))
+                   and(mul0 := self.mul()))
+        print(f"in add_0: {res}\nexiting add_0")
         if res:
-            return [add0, tok0, atom0]
+            return [add0, tok0, mul0]
         return None
 
     def add_1(self):
         print("enter add_1")
-        res = bool((add0 := self.memoize(self.add))
-            and(tok0 := self.term(Token.SUB))
-            and(atom0:= self.atom()))
-        print("in add_1")
-        print(f"res:{res}")
-        print("exiting add_1\n")
+        res = bool(   (add0 := self.left_rec_memoize(self.add))
+                   and(tok0 := self.term(Token.SUB))
+                   and(mul0 := self.mul()))
+        print(f"in add_1: {res}\nexiting add_1")
         if res:
-            return [add0, tok0, atom0]
+            return [add0, tok0, mul0]
         return None
 
     def add_2(self):
         print("enter add_2")
+        res = bool(mul0:= self.mul())
+        print(f"in add_2: {res}\nexiting add_2")
+        if res:
+            return [mul0]
+        return None
+
+    def mul(self):
+        print("enter mul")
+        save = self.mark()
+
+        self.reset(save)
+        if (prod:=self.mul_0()):
+            print("in mul: returning mul_0")
+            return PTNode("mul_0", prod)
+        self.reset(save)
+        if (prod:=self.mul_1()):
+            print("in mul: returning mul_0")
+            return PTNode("mul_1", prod)
+        self.reset(save)
+        if (prod:=self.mul_2()):
+            return PTNode("mul_2", prod)
+        self.reset(save)
+        return None
+
+    def mul_0(self):
+        print("enter mul_0")
+        res = bool(   (mul0 := self.left_rec_memoize(self.mul))
+                   and(tok0 := self.term(Token.MUL))
+                   and(atom0:= self.atom()))
+        print(f"in mul0: {res}\nexiting mul_0")
+        if res:
+            return [mul0, tok0, atom0]
+        return None
+
+    def mul_1(self):
+        print("enter mul_1")
+        res = bool(   (mul0 := self.left_rec_memoize(self.mul))
+                   and(tok0 := self.term(Token.DIV))
+                   and(atom0:= self.atom()))
+        print(f"in mul1: {res}\nexiting mul_1")
+        if res:
+            return [mul0, tok0, atom0]
+        return None
+
+    def mul_2(self):
+        print("enter mul_2")
         res = bool(atom0:= self.atom())
-        print("in add_2")
-        print(f"res:{res}")
-        print("exiting add_2\n")
+        print(f"in mul2: {res}\nexiting mul_2")
         if res:
             return [atom0]
         return None
@@ -135,20 +172,22 @@ class Parser:
 
         self.reset(save)
         if (prod:=self.atom_0()):
+            print("in atom: returning atom_0")
             return PTNode("atom_0", prod)
         self.reset(save)
         if (prod:=self.atom_1()):
+            print("in atom: returning atom_1")
             return PTNode("atom_1", prod)
+        self.reset(save)
+        print("in atom: returning None")
         return None
 
     def atom_0(self):
         print("enter atom_0")
         res = bool( (tok0:= self.term(Token.LPAREN))
-            and (add0:= self.memoize(self.add))
+            and (add0:= self.left_rec_memoize(self.add))
             and (tok1:= self.term(Token.RPAREN)))
-        print("in atom_0")
-        print(f"res:{res}")
-        print("exiting atom_0\n")
+        print(f"in atom_0: {res}\nexiting atom_0")
         if res:
             return [tok0, add0, tok1]
         return None
@@ -156,20 +195,13 @@ class Parser:
     def atom_1(self):
         print("enter atom_1")
         res = bool(tok0 := self.term(Token.NUMBER))
-        print("in atom_1")
-        print(f"res:{res}")
-        print("exiting atom_1\n")
+        print(f"in atom_1: {res}\nexiting atom_1")
         if res:
             return [tok0]
         return None
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath')
-    args = parser.parse_args()
-
-    inp = open(args.filepath).read()
+    inp = open("test.txt").read()
     lex = list(Lexer(inp))
 
     res, tree = Parser(lex).parse()
