c789c7c0d950bd9529adfdea995a73c3fca13531
Amadeus8182 <amadeus8182@gmail.com>
2023-12-08 19:29:00 -0600

    add benchmarking functions

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ mod lexer;
 mod parser;
 use lexer::lex::Lexer;
 use parser::parser::Parser;
-use std::{env, fs};
-use std::time::Instant;
+use std::{time::Instant, env, fs};
 
 fn main() {
     let paths: Vec<String> = env::args().skip(1).collect();
@@ -25,9 +24,9 @@ fn main() {
         let mut parser = Parser::new(lex);
         let now = Instant::now();
         let tree = parser.parse(false);
-        if let Some(tree) = tree {
+        if let Some(_tree) = tree {
             println!("pure packrat: {:?}", Instant::now()-now);
-            println!("{}", tree);
+            // println!("{}", tree);
         } else {
             println!("failure");
         }
@@ -36,12 +35,54 @@ fn main() {
         let mut parser = Parser::new(lex);
         let now = Instant::now();
         let tree = parser.parse(true);
-        if let Some(tree) = tree {
+        if let Some(_tree) = tree {
             println!("with pratt: {:?}", Instant::now()-now);
-            println!("{}", tree);
+            // println!("{}", tree);
         } else {
             println!("failure");
         }
         
+    }
+}
+
+#[test]
+fn benchmark_pure_packrat() {
+    use std::time::Duration;
+    let mut n = 1;
+    println!("Pure Packrat\nNumber of Lines,Time to Parse");
+    loop {
+        let input = "let a: bool = !((~(1 + 1) ^ ((1 * 1 + 1 / 1 ) >> 3)) == ((8 & 4 / (16 | 16)) & 255)) && ~((8*8)>>8) > 256 * ((8 + 8)>>12) + 64;";
+        let lex = Lexer::new(input.to_string().repeat(n));
+        let mut parser = Parser::new(lex);
+        let now = Instant::now();
+        let tree = parser.parse(false);
+        let t = Instant::now()-now;
+        println!("{},{:?}", n, t);
+        assert!(tree.is_some());
+        if t > Duration::new(600, 0) {
+            break
+        }
+        n *= 2;
+    }
+}
+
+#[test]
+fn benchmark_pratt() {
+    use std::time::Duration;
+    let mut n = 1;
+    println!("Pratt\nNumber of Lines,Time to Parse");
+    loop {
+        let input = "let a: bool = !((~(1 + 1) ^ ((1 * 1 + 1 / 1 ) >> 3)) == ((8 & 4 / (16 | 16)) & 255)) && ~((8*8)>>8) > 256 * ((8 + 8)>>12) + 64;";
+        let lex = Lexer::new(input.to_string().repeat(n));
+        let mut parser = Parser::new(lex);
+        let now = Instant::now();
+        let tree = parser.parse(true);
+        let t = Instant::now()-now;
+        println!("{},{:?}", n, t);
+        assert!(tree.is_some());
+        if t > Duration::new(600, 0) {
+            break
+        }
+        n *= 2;
     }
 }
